bdd1c01e5bd5ed23bfc360c5e77c7de42121dafa
riteshjadhav <jadhav.ritesh0416@gmail.com>
2023-11-16 16:39:00 +0530

    react-element-jsx

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const heading = React.createElement("h1",{},"hello");
-console.log(heading);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading)
 
+
+// React.createElement => object => HtmlElement(render)
+
+const heading = React.createElement("h1",{id: "heading"},"React element");
+
+// JSX - Html-likely syntax that understand by JS engine  
+// JSX => babel transpile it to React.createElement => ReactElement-JS Object => htmlElement(reder)
+
+const jsx = <h1 id="heading">React JSX element</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsx);
