226337fde2d312a06fd87c9d21b64e3112fa7ee7
riteshjadhav <jadhav.ritesh0416@gmail.com>
2023-11-21 13:48:09 +0530

    filtering-functionality

--- components/Body.js
+++ components/Body.js
@@ -1,15 +1,30 @@
 import dummyData from "../utils/mockData";
 import { swiggyCdn } from "../utils/constants";
 import Card from "./Card";
+import { useState } from "react";
+
 const Body = () => {
+  const [resturantList, setRestaurantList] = useState(dummyData);
+
+  console.log(resturantList);
   return (
     <div className="rest-container">
       <div className="search">
         <label htmlFor="search-box">Search </label>
         <input id="search-box" type="search" placeholder="Explore kitchens" />
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = resturantList.filter(
+              (restro) => restro.info.avgRating > 4.3
+            );
+          }}
+        >
+          Filter
+        </button>
       </div>
       <div className="card-container">
-        {dummyData.map((restro) => (
+        {resturantList.map((restro) => (
           <Card
             key={restro.info.id}
             name={restro.info.name}
